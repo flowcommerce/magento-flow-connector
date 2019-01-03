2623c2f3b6fb5901418c3af4b3f4b1d0d9dd9cb6
Marko Martinović <marko@devgenii.com>
2019-01-03 16:40:40 +0100

    Move get flow checkout url to session manager.

--- Observer/FlowConnectorCheckoutRedirectObserver.php
+++ Observer/FlowConnectorCheckoutRedirectObserver.php
@@ -4,10 +4,9 @@ namespace FlowCommerce\FlowConnector\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Magento\Checkout\Helper\Cart as CartHelper;
 use FlowCommerce\FlowConnector\Model\Configuration;
 use Magento\Framework\App\ActionFlag;
-use Magento\Framework\UrlInterface;
+use FlowCommerce\FlowConnector\Model\SessionManager;
 
 class FlowConnectorCheckoutRedirectObserver implements ObserverInterface
 {
@@ -17,48 +16,42 @@ class FlowConnectorCheckoutRedirectObserver implements ObserverInterface
     private $actionFlag;
 
     /**
-     * @var CartHelper
-     */
-    private $cartHelper;
-
-    /**
      * @var Configuration
      */
     private $configuration;
 
     /**
-     * @var UrlInterface
+     * @var SessionManager
      */
-    private $url;
+    private $sessionManager;
 
     /**
      * FlowConnectorCheckoutRedirectObserver constructor.
      * @param ActionFlag $actionFlag
-     * @param CartHelper $cartHelper
      * @param Configuration $configuration
-     * @param UrlInterface $url
+     * @param SessionManager $sessionManager
      */
     public function __construct(
         ActionFlag $actionFlag,
-        CartHelper $cartHelper,
         Configuration $configuration,
-        UrlInterface $url
+        SessionManager $sessionManager
     ) {
         $this->actionFlag = $actionFlag;
-        $this->cartHelper = $cartHelper;
         $this->configuration = $configuration;
-        $this->url = $url;
+        $this->sessionManager = $sessionManager;
     }
 
     /**
      * Redirect to checkout cart if Flow enabled
      * @param Observer $observer
      * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function execute(Observer $observer)
     {
-        if ($this->configuration->isFlowEnabled() && $this->configuration->isRedirectEnabled()) {
-            $redirectUrl = $this->url->getRedirectUrl($this->url->getUrl('flowconnector/checkout/redirecttoflow'));
+        if ($this->configuration->isFlowEnabled()
+            && $this->configuration->isRedirectEnabled()
+            && ($redirectUrl = $this->sessionManager->getCheckoutUrlWithCart())) {
             $this->actionFlag->set('', \Magento\Framework\App\Action\Action::FLAG_NO_DISPATCH, true);
             $observer->getControllerAction()->getResponse()->setRedirect($redirectUrl);
         }

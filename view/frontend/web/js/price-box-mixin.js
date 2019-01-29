7541fa1131e53d7b909e8cd513fdfe4c5ff5291d
David Mancuso <davidjmancuso@gmail.com>
2019-01-29 11:02:32 -0500

    refactor getting flow experience key

--- view/frontend/web/js/price-box-mixin.js
+++ view/frontend/web/js/price-box-mixin.js
@@ -8,24 +8,17 @@ define([
     'use strict';
 
     return function (widget) {
-        var flow = window.flow || {};
         var globalOptions = {
-                productId: null,
-                priceConfig: null,
-                prices: {},
                 priceTemplate: '<span class="price"><%- data.formatted %></span>',
                 flowPriceTemplateById: '<span data-flow-item-attribute-key="product_id" data-flow-item-attribute-value="<%- data.productId %>"><span data-flow-localize="item-price" class="price"><span style="width:3em; height:0.5em; display:inline-block;"></span></span></span>',
                 flowPriceTemplateBySkuPriceCode: '<span data-flow-item-number="<%- data.productSku %>"><span data-flow-localize="item-price-attribute" data-flow-item-price-attribute="<%- data.flowPriceCode %>" class="price"><span style="width:3em; height:0.5em; display:inline-block;"></span></span></span>'
             },
+            flow = window.flow || {},
             MAGENTOBASEPRICEKEY = 'basePrice',
             MAGENTOFINALPRICEKEY = 'finalPrice', 
             FLOWBASEPRICEKEY = 'base_price',
             FLOWREGULARPRICEKEY = 'regular_price',
-            FLOWFINALPRICEKEY = 'final_price',
-            FLOWEXPERIENCE = flow.session.getExperience(),
-            FLOWCOUNTRY = flow.session.getCountry(),
-            FLOWCURRENCY = flow.session.getCurrency(),
-            FLOWLOCALIZATIONKEY = FLOWEXPERIENCE+FLOWCOUNTRY+FLOWCURRENCY;
+            FLOWFINALPRICEKEY = 'final_price';
 
         $.widget('mage.priceBox', widget, {
             options: globalOptions,
@@ -33,7 +26,6 @@ define([
             reloadPrice: function reDrawPrices() {
                 var priceFormat = (this.options.priceConfig && this.options.priceConfig.priceFormat) || {},
                     priceTemplate = mageTemplate(this.options.priceTemplate);
-                this.hasFlowExperience = false;
                 this.flowFormattedPrice = false;
                 _.each(this.cache.displayPrices, function (price, priceCode) {
                     price.final = _.reduce(price.adjustments, function (memo, amount) {
@@ -44,10 +36,9 @@ define([
 
                     var template = { data: price };
                     
-                    if (typeof(FLOWEXPERIENCE) == "string") {
-                        var flow = window.flow || {};
+                    var flowLocalizationKey = this.getFlowExperience();
+                    if (flowLocalizationKey) {
                         flow.magento2 = window.flow.magento2 || {};
-                        this.hasFlowExperience = true;
                         var flowLocalizedPrices = false,
                             flowPriceCode = FLOWREGULARPRICEKEY;
                         switch (priceCode) {
@@ -67,11 +58,11 @@ define([
                         }
 
                         template.data.productId = this.getCurrentProductId(this.options.productId);
-                        template.data.productSku = this.getCurrentProductSku(template.data.productId, flowLocalizedPrices);
+                        template.data.productSku = this.getCurrentProductSku(template.data.productId, flowLocalizedPrices, flowLocalizationKey);
                         template.data.flowLocalized = false;
                         template.data.flowPriceCode = flowPriceCode;
 
-                        template = this.localizeTemplate(template, flowLocalizedPrices);
+                        template = this.localizeTemplate(template, flowLocalizedPrices, flowLocalizationKey);
 
                         if (!template.data.flowLocalized) {
                             priceTemplate = mageTemplate(this.options.flowPriceTemplateById);
@@ -83,9 +74,25 @@ define([
 
                     $('[data-price-type="' + priceCode + '"]', this.element).html(priceTemplate(template));
                 }, this);
-                if (this.hasFlowExperience && !this.flowFormattedPrice) {
+                if (flowLocalizationKey && !this.flowFormattedPrice) {
                     flow.cmd('localize');
                 }
+            },
+
+            getFlowExperience: function () {
+                var flowLocalizationKey = false;
+                if (flow.session != undefined) {
+                    var flowExperience = flow.session.getExperience(),
+                        flowCountry = flow.session.getCountry(),
+                        flowCurrency = flow.session.getCurrency();
+                    if (typeof(flowExperience) == "string" &&
+                        typeof(flowCountry) == "string" &&
+                        typeof(flowCurrency) == "string"
+                    ) {
+                        flowLocalizationKey = flowExperience+flowCountry+flowCurrency;
+                    }
+                }
+                return flowLocalizationKey;
             },
 
             getCurrentProductId: function (productId) {
@@ -103,24 +110,24 @@ define([
                 return productId;
             },
 
-            getCurrentProductSku: function (productId, flowLocalizedPrices) {
+            getCurrentProductSku: function (productId, flowLocalizedPrices, flowLocalizationKey) {
                 if (flowLocalizedPrices) {
-                    if (flowLocalizedPrices[FLOWLOCALIZATIONKEY][productId]['sku'] != undefined) {
-                        return flowLocalizedPrices[FLOWLOCALIZATIONKEY][productId]['sku'];
+                    if (flowLocalizedPrices[flowLocalizationKey][productId]['sku'] != undefined) {
+                        return flowLocalizedPrices[flowLocalizationKey][productId]['sku'];
                     }
                 }
                 return false;
             },
 
-            localizeTemplate: function (template, flowLocalizedPrices) {
-                if (flowLocalizedPrices[FLOWLOCALIZATIONKEY][template.data.productId][FLOWFINALPRICEKEY] != undefined) {
-                    if (flowLocalizedPrices[FLOWLOCALIZATIONKEY][template.data.productId][FLOWFINALPRICEKEY].label != undefined) {
-                        this.flowFormattedPrice = flowLocalizedPrices[FLOWLOCALIZATIONKEY][template.data.productId][FLOWFINALPRICEKEY].label;
+            localizeTemplate: function (template, flowLocalizedPrices, flowLocalizationKey) {
+                if (flowLocalizedPrices[flowLocalizationKey][template.data.productId][FLOWFINALPRICEKEY] != undefined) {
+                    if (flowLocalizedPrices[flowLocalizationKey][template.data.productId][FLOWFINALPRICEKEY].label != undefined) {
+                        this.flowFormattedPrice = flowLocalizedPrices[flowLocalizationKey][template.data.productId][FLOWFINALPRICEKEY].label;
                     }
                 }
-                if (flowLocalizedPrices[FLOWLOCALIZATIONKEY][template.data.productId][template.data.flowPriceCode] != undefined) {
-                    if (flowLocalizedPrices[FLOWLOCALIZATIONKEY][template.data.productId][template.data.flowPriceCode].label != undefined) {
-                        this.flowFormattedPrice = flowLocalizedPrices[FLOWLOCALIZATIONKEY][template.data.productId][template.data.flowPriceCode].label;
+                if (flowLocalizedPrices[flowLocalizationKey][template.data.productId][template.data.flowPriceCode] != undefined) {
+                    if (flowLocalizedPrices[flowLocalizationKey][template.data.productId][template.data.flowPriceCode].label != undefined) {
+                        this.flowFormattedPrice = flowLocalizedPrices[flowLocalizationKey][template.data.productId][template.data.flowPriceCode].label;
                     }
                 }
                 if (this.flowFormattedPrice) {

c36b110286bee06a7cff71765803ea21af450b63
David Mancuso <davidjmancuso@gmail.com>
2019-02-19 14:07:09 -0500

    refactor country picker for better extension

--- view/frontend/web/js/flow-country-picker.js
+++ view/frontend/web/js/flow-country-picker.js
@@ -3,18 +3,17 @@ require([
     'flowCountryPicker',
     'mage/cookies'
 ], function ($) {
-    var containerId = 'flow-country-picker';
+    var flow = window.flow || {};
+        flow.magento2 = window.flow.magento2 || {};
+        flow.magento2.countryPickerOptions = window.flow.magento2.countryPickerOptions || {};
+        flow.magento2.countryPickerOptions.containerId = 'flow-country-picker';
+        flow.magento2.countryPickerOptions.type = 'dropdown';
+        flow.magento2.countryPickerOptions.logo = true;
+        flow.magento2.countryPickerOptions.isDestination = true;
+        flow.magento2.countryPickerOptions.onSessionUpdate = function (status, session) {
+            $.cookie('flow_mage_session_update', 1,  {domain: null});
+            window.location.reload();
+        };
 
-    if ($('#'.containerId).length > 0) {
-        window.flow.countryPicker.createCountryPicker({
-            type: 'dropdown',
-            logo: true,
-            containerId: containerId,
-            onSessionUpdate: function (status, session) {
-                $.cookie('flow_mage_session_update', 1,  {domain: null});
-                window.location.reload();
-            },
-            isDestination: true
-        });
-    }
+    window.flow.countryPicker.createCountryPicker(flow.magento2.countryPickerOptions);
 });

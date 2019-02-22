99a0b16039d1191efb5f89fd243b168779459d52
David Mancuso <davidjmancuso@gmail.com>
2019-02-22 15:53:45 -0500

    line item qty fixed, todo summary trigger/subscription

--- view/frontend/web/js/shopping-cart-mixin.js
+++ view/frontend/web/js/shopping-cart-mixin.js
@@ -1,6 +1,7 @@
 define([
     'jquery',
-], function ($) {
+    'Magento_Checkout/js/model/totals'
+], function ($, totalsService) {
     'use strict';
 
     return function (widget) {
@@ -22,30 +23,37 @@ define([
                     discount;
                 if (typeof(flow.session.getExperience()) == 'string') {
                     cartContainer = $('.cart-container').first();
-                    totals = cartContainer.find('#cart-totals');
-                    subtotal = totals.find('[data-th=\'Subtotal\'] span.price').first();
-                    grandtotal = totals.find('[data-th=\'Order Total\'] span.price').first();
-                    discount = totals.find('[data-th=\'Discount\'] span.price').first();
-
                     cartContainer.attr('data-flow-cart-container', '');
-                    subtotal.attr('data-flow-localize','cart-subtotal'); 
-                    grandtotal.attr('data-flow-localize','cart-total'); 
-                    discount.attr('data-flow-localize','cart-discount'); 
 
                     items = cartContainer.find('[data-role="cart-item-qty"]');
                     for (i = 0; i < items.length; i++) {
                         var qty, number, itemContainer;
-                        qty = $(items[i]).value;
+                        qty = $(items[i]).attr('value');
                         number = $(items[i]).data('cart-item-id');
                         itemContainer = $(items[i]).closest('.cart.item');
-                        itemContainer.attr({
-                            'data-flow-cart-item-number': number,
-                            'data-flow-cart-item-quantity': qty
-                        });
+                        itemContainer.attr('data-flow-cart-item-number', number);
+                        itemContainer.attr('data-flow-cart-item-quantity', qty);
                         itemContainer.find('.price .cart-price > span.price').first().attr('data-flow-localize','cart-item-price');
                         itemContainer.find('.subtotal .cart-price > span.price').first().attr('data-flow-localize','cart-item-line-total');
                     }
 
+                    totalsService.totals.subscribe(function () {
+                        console.log('totals updated');
+
+                        totals = cartContainer.find('#cart-totals');
+                        subtotal = totals.find('[data-th=\'Subtotal\'] span.price').first();
+                        grandtotal = totals.find('[data-th=\'Order Total\'] span.price').first();
+                        discount = totals.find('[data-th=\'Discount\'] span.price').first();
+
+                        subtotal.attr('data-flow-localize','cart-subtotal'); 
+                        grandtotal.attr('data-flow-localize','cart-total'); 
+                        discount.attr('data-flow-localize','cart-discount'); 
+
+                        flow.cart.localize();
+                        console.log('totals localized');
+                    });
+
+                    console.log('cart localize init');
                     flow.cart.localize();
                 }
 

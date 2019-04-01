2df6fb898510930a056d7726f9230ebea888cf36
flowio-david <davidjmancuso@gmail.com>
2019-04-01 16:34:01 -0400

    move some commands from build to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,4 +20,18 @@ echo -e '\nSetEnvIf X-Forwarded-Proto https HTTPS=on' >> .htaccess
 nami_initialize apache php mysql-client libphp magento
 info "Starting magento... "
 
+php bin/magento maintenance:enable
+php bin/magento app:config:dump
+php bin/magento setup:static-content:deploy -f
+php bin/magento module:enable --all
+php bin/magento setup:upgrade
+php bin/magento setup:di:compile
+php bin/magento cache:flush
+php bin/magento sampledata:deploy
+php bin/magento maintenance:disable
+
+find . -type d -print0 | xargs -0 chmod 775 && \
+find . -type f -print0 | xargs -0 chmod 664 && \
+chown -R bitnami:daemon /opt/bitnami/magento/htdocs
+
 exec tini -- "$@"

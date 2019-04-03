1ed48ff6d207ad0c9dbf06003d93316be1706eeb
flowio-david <davidjmancuso@gmail.com>
2019-04-03 13:53:15 -0400

    entrypoint order of operations

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,14 @@ echo "NAMI_LOG_LEVEL=$NAMI_LOG_LEVEL"
 usermod -aG sudo bitnami
 
 . /init.sh
-chown -R bitnami:daemon /opt/bitnami/magento/htdocs/var
-chown -R bitnami:daemon /opt/bitnami/magento/htdocs/generated
-chown -R bitnami:daemon /opt/bitnami/magento/htdocs/app/etc
+rm -rf /opt/bitnami/magento/htdocs/var/cache/*
+find /opt/bitnami/magento/htdocs -type d -print0 | xargs -0 chmod 775
+find /opt/bitnami/magento/htdocs -type f -print0 | xargs -0 chmod 664
+chown -R bitnami:daemon /opt/bitnami/magento/htdocs/var \
+                        /opt/bitnami/magento/htdocs/generated \
+                        /opt/bitnami/magento/htdocs/app/etc
 echo -e '\nSetEnvIf X-Forwarded-Proto https HTTPS=on' >> .htaccess
+runuser -l bitnami -c "/opt/bitnami/php/bin/php /opt/bitnami/magento/htdocs/bin/magento maintenance:disable"
 
 nami_initialize apache php mysql-client libphp magento
 info "Starting magento... "
@@ -25,14 +29,7 @@ runuser -l bitnami -c "/opt/bitnami/php/bin/php /opt/bitnami/magento/htdocs/bin/
 runuser -l bitnami -c "/opt/bitnami/php/bin/php /opt/bitnami/magento/htdocs/bin/magento config:set web/unsecure/base_url \"https://$MAGENTO_BASE_URL/\""
 runuser -l bitnami -c "/opt/bitnami/php/bin/php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/use_in_frontend 1"
 runuser -l bitnami -c "/opt/bitnami/php/bin/php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/use_in_adminhtml 1"
-
-echo "Re-apply permissions..."
-find /opt/bitnami/magento/htdocs -type d -exec chmod 775 {} \;
-find /opt/bitnami/magento/htdocs -type f -exec chmod 664 {} \;
-rm -rf /opt/bitnami/magento/htdocs/var/cache/*
-runuser -l bitnami -c "/opt/bitnami/php/bin/php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile"
 runuser -l bitnami -c "/opt/bitnami/php/bin/php /opt/bitnami/magento/htdocs/bin/magento cache:clean"
 runuser -l bitnami -c "/opt/bitnami/php/bin/php /opt/bitnami/magento/htdocs/bin/magento cache:flush"
-chown -R bitnami:daemon /opt/bitnami/magento/htdocs
 
 exec tini -- "$@"

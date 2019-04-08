1d678456886bd2c10fe494cea82fe2316d41ea1f
flowio-david <davidjmancuso@gmail.com>
2019-04-08 09:01:14 -0400

    proper implementation of a requirejs component suppreses constr error

--- view/frontend/web/js/view/minicart-mixin.js
+++ view/frontend/web/js/view/minicart-mixin.js
@@ -5,39 +5,42 @@ define([
     'use strict';
 
     return function (Component) {
-        var miniCart,
-            body,
-            flowMiniCartLocalize;
+        return Component.extend({
+            initialize: function () {
+                var miniCart,
+                    body,
+                    flowMiniCartLocalize,
+                    result = this._super();
 
-        if (!window.flow.magento2.shouldLocalizeCart) {
-            return false;
-        }
+                if (!window.flow.magento2.shouldLocalizeCart) {
+                    return result;
+                }
 
-        miniCart = $('[data-block=\'minicart\']');
-        body = $('[data-container=\'body\']');
-        window.flow.magento2.miniCartAvailable = false;
-        flowMiniCartLocalize = function (source, waitTimeMs = 0) {
-            window.flow.magento2.hideCart();
-            window.flow.magento2.hideCartTotals();
-            setTimeout(function(){
-                window.flow.cart.localize()
-                console.log('Flow localizing cart: ' + source + ', waited: ' + waitTimeMs + 'ms');
-            }, waitTimeMs);
-            return true;
-        };
+                miniCart = $('[data-block=\'minicart\']');
+                body = $('[data-container=\'body\']');
+                window.flow.magento2.miniCartAvailable = false;
+                flowMiniCartLocalize = function (source, waitTimeMs = 0) {
+                    window.flow.magento2.hideCart();
+                    window.flow.magento2.hideCartTotals();
+                    setTimeout(function(){
+                        window.flow.cart.localize()
+                        console.log('Flow localizing cart: ' + source + ', waited: ' + waitTimeMs + 'ms');
+                    }, waitTimeMs);
+                };
 
-        if (body.hasClass('checkout-cart-index')) {
-            // Is Cart page
-            body.addClass('flow-cart');
-            miniCart.remove();
-        } else {
-            // Is not Cart page
-            window.flow.magento2.miniCartAvailable = true;
-            miniCart.attr('data-flow-cart-container', '');
-            miniCart.on('dropdowndialogopen', function(){flowMiniCartLocalize('open')});
-            miniCart.on('contentUpdated', function(){flowMiniCartLocalize('minicart.contentUpdated', 500)});
-        }
-
-        return Component.extend();
+                if (body.hasClass('checkout-cart-index')) {
+                    // Is Cart page
+                    body.addClass('flow-cart');
+                    miniCart.remove();
+                } else {
+                    // Is not Cart page
+                    window.flow.magento2.miniCartAvailable = true;
+                    miniCart.attr('data-flow-cart-container', '');
+                    miniCart.on('dropdowndialogopen', function(){flowMiniCartLocalize('open')});
+                    miniCart.on('contentUpdated', function(){flowMiniCartLocalize('minicart.contentUpdated', 500)});
+                }
+                return result;
+            }
+        });
     }
 });

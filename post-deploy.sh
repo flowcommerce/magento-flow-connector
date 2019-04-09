2c877362ef29e79601926a8b46c35a5e7be5598c
flowio-david <davidjmancuso@gmail.com>
2019-04-09 14:29:30 -0400

    include generated dir and permissions update in post-deploy

--- post-deploy.sh
+++ post-deploy.sh
@@ -4,6 +4,7 @@ M2_ROOT="/opt/bitnami/magento/htdocs/"
 rm -rf \
     ${M2_ROOT}var/di/* \
     ${M2_ROOT}var/generation/* \
+    ${M2_ROOT}var/generated/* \
     ${M2_ROOT}var/cache/* \
     ${M2_ROOT}var/page_cache/* \
     ${M2_ROOT}var/view_preprocessed/* \
@@ -16,3 +17,7 @@ php ${M2_ROOT}bin/magento setup:di:compile
 php ${M2_ROOT}bin/magento setup:static-content:deploy -f
 php ${M2_ROOT}bin/magento cache:clean
 php ${M2_ROOT}bin/magento cache:flush
+
+find . -type d -print0 | xargs -0 chmod 775
+find . -type f -print0 | xargs -0 chmod 664
+chown -R bitnami:daemon .

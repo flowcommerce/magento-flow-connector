926687a78aa72b6503dd6c35ec9fb0ab1a708194
flowio-david <davidjmancuso@gmail.com>
2019-04-10 13:55:06 -0400

    change root of permissions updates in post-deploy

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,26 +1,24 @@
 #!/bin/bash -e
 M2_ROOT="/opt/bitnami/magento/htdocs/"
 
-# rm -rf \
-#     ${M2_ROOT}var/di/* \
-#     ${M2_ROOT}var/generation/* \
-#     ${M2_ROOT}var/generated/* \
-#     ${M2_ROOT}var/cache/* \
-#     ${M2_ROOT}var/page_cache/* \
-#     ${M2_ROOT}var/view_preprocessed/* \
-#     ${M2_ROOT}var/composer_home/cache/*
+rm -rf \
+    ${M2_ROOT}var/di/* \
+    ${M2_ROOT}var/generation/* \
+    ${M2_ROOT}var/generated/* \
+    ${M2_ROOT}var/cache/* \
+    ${M2_ROOT}var/page_cache/* \
+    ${M2_ROOT}var/view_preprocessed/* \
+    ${M2_ROOT}var/composer_home/cache/*
 
 cp ${M2_ROOT}vendor/magento/module-cms-sample-data/fixtures/styles.css ${M2_ROOT}pub/media/styles.css
-chmod 775 ${M2_ROOT}pub/media/styles.css
-chown bitnami:daemon ${M2_ROOT}pub/media/styles.css
 
 php ${M2_ROOT}bin/magento config:set web/unsecure/base_url "https://$MAGENTO_BASE_URL/"
 php ${M2_ROOT}bin/magento indexer:reindex
-# php ${M2_ROOT}bin/magento setup:di:compile
-# php ${M2_ROOT}bin/magento setup:static-content:deploy -f
+php ${M2_ROOT}bin/magento setup:di:compile
+php ${M2_ROOT}bin/magento setup:static-content:deploy -f
 php ${M2_ROOT}bin/magento cache:clean
 php ${M2_ROOT}bin/magento cache:flush
 
-# find . -type d -print0 | xargs -0 chmod 775
-# find . -type f -print0 | xargs -0 chmod 664
-# chown -R bitnami:daemon .
+find ${M2_ROOT} -type d -print0 | xargs -0 chmod 775
+find ${M2_ROOT} -type f -print0 | xargs -0 chmod 664
+chown -R bitnami:daemon ${M2_ROOT}

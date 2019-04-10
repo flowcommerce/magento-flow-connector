bb17b1b38a677076567572d3d2de8061e7721417
flowio-david <davidjmancuso@gmail.com>
2019-04-10 10:37:56 -0400

    simpler deploy

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 M2_ROOT="/opt/bitnami/magento/htdocs/"
 
-rm -rf \
-    ${M2_ROOT}var/di/* \
-    ${M2_ROOT}var/generation/* \
-    ${M2_ROOT}var/generated/* \
-    ${M2_ROOT}var/cache/* \
-    ${M2_ROOT}var/page_cache/* \
-    ${M2_ROOT}var/view_preprocessed/* \
-    ${M2_ROOT}var/composer_home/cache/*
+# rm -rf \
+#     ${M2_ROOT}var/di/* \
+#     ${M2_ROOT}var/generation/* \
+#     ${M2_ROOT}var/generated/* \
+#     ${M2_ROOT}var/cache/* \
+#     ${M2_ROOT}var/page_cache/* \
+#     ${M2_ROOT}var/view_preprocessed/* \
+#     ${M2_ROOT}var/composer_home/cache/*
 
 cp ${M2_ROOT}vendor/magento/module-cms-sample-data/fixtures/styles.css ${M2_ROOT}pub/media/styles.css
 
 php ${M2_ROOT}bin/magento config:set web/unsecure/base_url "https://$MAGENTO_BASE_URL/"
 php ${M2_ROOT}bin/magento indexer:reindex
-php ${M2_ROOT}bin/magento setup:di:compile
-php ${M2_ROOT}bin/magento setup:static-content:deploy -f
+# php ${M2_ROOT}bin/magento setup:di:compile
+# php ${M2_ROOT}bin/magento setup:static-content:deploy -f
 php ${M2_ROOT}bin/magento cache:clean
 php ${M2_ROOT}bin/magento cache:flush
 

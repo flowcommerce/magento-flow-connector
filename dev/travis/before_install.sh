edc83edef3c4732ee3fc42e2275ff16e445193cb
flowio-david <davidjmancuso@gmail.com>
2019-04-15 13:47:24 -0400

    install rabbitmq as service in yml

--- dev/travis/before_install.sh
+++ dev/travis/before_install.sh
@@ -7,10 +7,3 @@ echo 'memory_limit = -1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d
 echo '==> Disabling xdebug'
 phpenv config-rm xdebug.ini
 phpenv rehash;
-echo '==> Install RabbitMQ Server'
-wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
-sudo apt-key add erlang_solutions.asc
-sudo apt-get update
-sudo apt-get install erlang
-sudo apt-get install erlang-nox
-sudo dpkg -i rabbitmq-server_3.2.1-1_all.deb

80baaa77aa06eead6cee98bf7de608d1e9002ed2
flowio-david <davidjmancuso@gmail.com>
2019-04-15 13:36:48 -0400

    extended install rabbitmq process

--- dev/travis/before_install.sh
+++ dev/travis/before_install.sh
@@ -8,4 +8,11 @@ echo '==> Disabling xdebug'
 phpenv config-rm xdebug.ini
 phpenv rehash;
 echo '==> Install RabbitMQ Server'
-sudo apt install -y rabbitmq-server;
+echo "deb http://www.rabbitmq.com/debian/ testing main"  | sudo tee  /etc/apt/sources.list.d/rabbitmq.list > /dev/null
+wget https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+sudo apt-key add rabbitmq-signing-key-public.asc
+sudo apt-get update
+sudo apt-get install rabbitmq-server -y
+sudo service rabbitmq-server start
+sudo rabbitmq-plugins enable rabbitmq_management
+sudo service rabbitmq-server restart

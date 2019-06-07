894f4b4d495f76802c15fd82a9b7b77489b57676
flowio-david <davidjmancuso@gmail.com>
2019-06-07 18:54:36 -0400

    add change event handler

--- view/frontend/web/js/configurable-mixin.js
+++ view/frontend/web/js/configurable-mixin.js
@@ -32,6 +32,33 @@ define([
                     window.flow.magento2.optionsIndex[productId] = optionIndex;
                 }
                 return this._super();
+            },
+
+            _configureElement: function (element) {
+                if (window.flow.magento2.shouldLocalizeCatalog) {
+                    var productId = $widget.options.jsonConfig.productId,
+                        selectedOptionId = element.config.id;
+                        optionsMap = _.toArray($widget.optionsMap);
+                    if (window.flow.magento2.optionsSelected == undefined) {
+                        window.flow.magento2.optionsSelected = [];
+                    }
+                    if (window.flow.magento2.optionsSelected[productId] == undefined) {
+                        window.flow.magento2.optionsSelected[productId] = [];
+                        _.each(_.toArray($widget.optionsMap), function(option, key) {
+                            window.flow.magento2.optionsSelected[productId].push(false);
+                        });
+                    }
+                    _.each(optionsMap, function(options, key) {
+                        if (typeof(options[selectedOptionId]) == "object") {
+                            if (window.flow.magento2.optionsSelected[productId][key] != selectedOptionId) {
+                                window.flow.magento2.optionsSelected[productId][key] = selectedOptionId; 
+                            } else {
+                                window.flow.magento2.optionsSelected[productId][key] = false;
+                            }
+                        }
+                    });
+                }
+                return this._super(element);
             }
         });
     }

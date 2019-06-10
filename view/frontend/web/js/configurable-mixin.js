241baf542853790a7223d93c24719d5a5a6d2131
flowio-david <davidjmancuso@gmail.com>
2019-06-10 12:05:43 -0400

    simplified localization for spConfig products

--- view/frontend/web/js/configurable-mixin.js
+++ view/frontend/web/js/configurable-mixin.js
@@ -12,51 +12,9 @@ define([
 
     return function (widget) {
         $.widget('mage.configurable', widget, {
-            _initializeOptions: function () {
-                if (window.flow.magento2.shouldLocalizeCatalog) {
-                    if (window.flow.magento2.optionsIndex == undefined) {
-                        window.flow.magento2.optionsIndex = {};
-                    }
-                    var productId = this.options.spConfig.productId;
-                    var optionIndex = [];
-                    _.each(this.options.spConfig.index, function (option, key) {
-                        var optionData = {
-                            productId: key,
-                            optionIds: []
-                        };
-                        _.each(option, function(value) {
-                            optionData.optionIds.push(value);
-                        });
-                        optionIndex.push(optionData);
-                    });
-                    window.flow.magento2.optionsIndex[productId] = optionIndex;
-                }
-                return this._super();
-            },
-
             _configureElement: function (element) {
                 if (window.flow.magento2.shouldLocalizeCatalog) {
-                    var productId = $widget.options.jsonConfig.productId,
-                        selectedOptionId = element.config.id;
-                        optionsMap = _.toArray($widget.optionsMap);
-                    if (window.flow.magento2.optionsSelected == undefined) {
-                        window.flow.magento2.optionsSelected = [];
-                    }
-                    if (window.flow.magento2.optionsSelected[productId] == undefined) {
-                        window.flow.magento2.optionsSelected[productId] = [];
-                        _.each(_.toArray($widget.optionsMap), function(option, key) {
-                            window.flow.magento2.optionsSelected[productId].push(false);
-                        });
-                    }
-                    _.each(optionsMap, function(options, key) {
-                        if (typeof(options[selectedOptionId]) == "object") {
-                            if (window.flow.magento2.optionsSelected[productId][key] != selectedOptionId) {
-                                window.flow.magento2.optionsSelected[productId][key] = selectedOptionId; 
-                            } else {
-                                window.flow.magento2.optionsSelected[productId][key] = false;
-                            }
-                        }
-                    });
+                    window.flow.magento2.simpleProduct = this._getSimpleProductId(element);
                 }
                 return this._super(element);
             }

1f1f677f4eee1e55b50b60988cc17f46b1c696b0
flowio-david <davidjmancuso@gmail.com>
2019-09-23 15:09:10 -0400

    fix error with console logger

--- Console/Command/FlowConsoleLogger.php
+++ Console/Command/FlowConsoleLogger.php
@@ -9,7 +9,7 @@ class FlowConsoleLogger extends ConsoleLogger
 
     const DATE_FORMAT = 'l jS F, Y - H:i:s';
 
-    public function log($level, $message, $context = [])
+    public function log($level, $message, array $context = [])
     {
         list($usec, $sec) = explode(' ', microtime());
         $msg = date('Y/m/d H:i:s', $sec) . substr($usec, 1) . ': ' . $message;

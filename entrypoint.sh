7bef1825e123a152dc08b561828d0a8736345b7b
flowio-david <davidjmancuso@gmail.com>
2019-10-17 14:36:43 -0400

    execute magento-init

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,14 @@ M2_ROOT="/opt/bitnami/magento/htdocs/"
 print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run.sh" ]]; then
-    # . /apache-init.sh
-    # . /magento-init.sh
 
     find ${M2_ROOT} -type d -print0 | xargs -0 chmod 775
     find ${M2_ROOT} -type f -print0 | xargs -0 chmod 664
     chown -RH bitnami:daemon ${M2_ROOT}
     echo -e '\nSetEnvIf X-Forwarded-Proto https HTTPS=on' >> .htaccess
+
+    chmod +x /magento-init.sh
+    /magento-init.sh
 
     nami_initialize apache php mysql-client libphp magento
     info "Starting magento... "

bf591a510de99505a46dc70c651c840bf575921a
flowio-david <davidjmancuso@gmail.com>
2019-10-18 09:35:17 -0400

    move htaccess updates to dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,6 @@ M2_ROOT="/opt/bitnami/magento/htdocs/"
 print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run.sh" ]]; then
-    echo -e "\nSetEnvIf X-Forwarded-Proto https HTTPS=on" >> /opt/bitnami/apache/conf/vhosts/htaccess/magento-htaccess.conf
-    echo -e "\nServerName $MAGENTO_BASE_URL" >> /opt/bitnami/apache/conf/vhosts/htaccess/magento-htaccess.conf
     . /magento-init.sh
 
     nami_initialize apache php mysql-client libphp magento

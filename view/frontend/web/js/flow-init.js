23789a29704646bc40e85ee46fdf3056c503a33e
flowio-david <david@flow.io>
2020-06-24 11:31:29 -0400

    wip

---
+++ view/frontend/web/js/flow-init.js
@@ -0,0 +1,6 @@
+define([], function () {
+    window['flow'] = window['flow'] || {};
+    window['flow'].cmd = window['flow'].cmd || function () {
+        (window['flow'].q = window['flow'].q || []).push(arguments);
+    };
+});

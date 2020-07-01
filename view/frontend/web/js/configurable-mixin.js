dc708f434074c448883dcfaa5568c87b1d327335
flowio-david <david@flow.io>
2020-07-01 11:18:57 -0400

    wip

--- view/frontend/web/js/configurable-mixin.js
+++ view/frontend/web/js/configurable-mixin.js
@@ -13,9 +13,10 @@ define([
     return function (widget) {
         $.widget('mage.configurable', widget, {
             _configureElement: function (element) {
+                var self = this;
                 flow.cmd('on', 'ready', function () {
                     if (flow.magento2.shouldLocalizeCatalog()) {
-                        flow.magento2.simpleProduct = this._getSimpleProductId(element);
+                        flow.magento2.simpleProduct = self._getSimpleProductId(element);
                     }
                 });
                 return this._super(element);

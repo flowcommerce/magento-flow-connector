636bb8be96b101269108cee514322d6e37e7fc21
flowio-david <david@flow.io>
2020-09-22 14:28:49 -0400

    wip

--- Controller/Webhooks/Base.php
+++ Controller/Webhooks/Base.php
@@ -122,7 +122,7 @@ abstract class Base extends \Magento\Framework\App\Action\Action implements Csrf
         $this->eventManager->dispatch($eventName, [
             'type' => $this->getEventType(),
             'payload' => $payload,
-            'store_if' => $storeId,
+            'storeId' => $storeId,
             'logger' => $this->logger
         ]);
 

8c65b6ab0d8f501425586299fff555dd827db299
flowio-david <david@flow.io>
2020-10-01 15:08:13 -0400

    wip

---
+++ Controller/Adminhtml/SyncOrder/View.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace FlowCommerce\FlowConnector\Controller\Adminhtml\SyncOrder;
+
+use FlowCommerce\FlowConnector\Model\SyncOrderFactory;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Backend\Model\View\Result\Page;
+use Magento\Backend\Model\View\Result\Redirect;
+use Magento\Framework\App\Request\DataPersistorInterface as DataPersistor;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class View
+ * @package FlowCommerce\FlowConnector\Controller\Adminhtml\SyncOrder
+ */
+class View extends Action
+{
+    /**
+     * Authorization level of a basic admin session
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Magento_Backend::system';
+
+    /**
+     * Registry Key - Order Sync
+     */
+    const REGISTRY_KEY_SYNC_ORDER = 'flowcommerce_flowconnector_sync_order';
+
+    /**
+     * @var Registry
+     */
+    private $registry;
+
+    /**
+     * @var DataPersistor
+     */
+    private $dataPersistor;
+
+    /**
+     * @var PageFactory
+     */
+    private $resultPageFactory;
+
+    /**
+     * @var SyncOrderFactory
+     */
+    private $syncOrderFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     * @param DataPersistor $dataPersistor
+     * @param SyncOrderFactory $syncOrderFactory
+     * @param Registry $registry
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        DataPersistor $dataPersistor,
+        SyncOrderFactory $syncOrderFactory,
+        Registry $registry
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+        $this->dataPersistor = $dataPersistor;
+        $this->syncOrderFactory = $syncOrderFactory;
+        $this->registry = $registry;
+    }
+
+    /**
+     * View action
+     * @return Page|Redirect
+     */
+    public function execute()
+    {
+        $value = $this->getRequest()->getParam('value');
+        $syncOrder = $this->syncOrderFactory->create();
+
+        if ($value) {
+            $syncOrder->load($value);
+            if (!$syncOrder->getValue()) {
+                $this->messageManager->addError(__('This Order Sync no longer exists.'));
+                /** @var Redirect $resultRedirect */
+                $resultRedirect = $this->resultRedirectFactory->create();
+                return $resultRedirect->setPath('*/*/');
+            }
+        }
+
+        $this->registry->register(self::REGISTRY_KEY_SYNC_ORDER, $syncOrder);
+
+        /** @var Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('FlowConnector::sync_order');
+        $resultPage->addBreadcrumb(__('Flow Connector'), __('Flow Connector'));
+        $resultPage->addBreadcrumb(__('Order Sync'), __('Order Sync'));
+        $resultPage->addBreadcrumb(__('View'), __('View'));
+        $resultPage->getConfig()->getTitle()->prepend(__('View Order Sync'));
+
+        return $resultPage;
+    }
+}
---
+++ Model/SyncOrderManager.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace FlowCommerce\FlowConnector\Model;
+
+use Exception;
+use FlowCommerce\FlowConnector\Api\Data\SyncOrderInterface;
+use FlowCommerce\FlowConnector\Api\SyncOrderManagementInterface;
+use FlowCommerce\FlowConnector\Model\Allocation as FlowAllocation;
+use FlowCommerce\FlowConnector\Model\Order as FlowOrder;
+use FlowCommerce\FlowConnector\Model\SyncManager;
+use FlowCommerce\FlowConnector\Model\WebhookEvent;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable as ConfigurableType;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\StoreManager;
+use Psr\Log\LoggerInterface as Logger;
+
+/**
+ * Class SyncOrderManagement
+ * @package FlowCommerce\FlowConnector\Model
+ */
+class SyncOrderManager implements SyncOrderManagementInterface
+{
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    /**
+     * @var StoreManager
+     */
+    private $storeManager;
+
+    /*
+     * @var SyncManager
+     */
+    private $syncManager;
+
+    /**
+     * @var FlowOrder
+     */
+    private $flowOrder;
+
+    /**
+     * @var FlowAllocation
+     */
+    private $flowAllocation;
+
+    /**
+     * @var WebhookEvent
+     */
+    private $webhookEvent;
+
+    /**
+     * SyncOrderManagement constructor.
+     * @param Logger $logger
+     * @param StoreManager $storeManager
+     * @param WebhookEvent $webhookEvent
+     * @param SyncManager $syncManager
+     * @param FlowOrder $flowOrder
+     * @param FlowAllocation $flowAllocation
+     */
+    public function __construct(
+        Logger $logger,
+        StoreManager $storeManager,
+        WebhookEvent $webhookEvent,
+        SyncManager $syncManager,
+        FlowOrder $flowOrder,
+        FlowAllocation $flowAllocation,
+    ) {
+        $this->logger = $logger;
+        $this->storeManager = $storeManager;
+        $this->webhookEvent = $webhookEvent;
+        $this->syncManager = $syncManager;
+        $this->flowOrder = $flowOrder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process()
+    {
+        $this->logger->info('Starting processing order sync poll');
+
+        foreach ($this->storeManager->getStores() as $store) {
+            $pendingOrderRecords = $this->syncManager->getSyncStreamPendingRecordByKey($store->getId(), $this->syncManager::PLACED_ORDER_TYPE);
+            if ((int) count($pendingOrderRecords) === 0) {
+                $this->logger->info('No orders found to process.');
+                return false;
+            }
+
+            foreach ($pendingOrderRecords as $pendingOrderRecord) {
+                $this->syncByValue($pendingOrderRecord['value']);
+            }
+        }
+        $this->logger->info('Done processing order sync poll');
+        return true;
+    }
+
+    /**
+     * Sync order by Flow Order Number
+     * @param string $value
+     * @return void
+     * @throws
+     */
+    public function syncByValue($value)
+    {
+        $this->logger->info('Processing pending Flow order number: ' . $orderNumber);
+        if ($this->webhookEvent->getOrderByFlowOrderNumber($orderNumber)) {
+            $this->logger->info('Flow order number: ' . $orderNumber . ' already imported.');
+            $this->syncManager->putSyncStreamRecord($store->getId(), $this->syncManager::PLACED_ORDER_TYPE, $orderNumber);
+            continue;
+        }
+        $order = $this->flowOrder->getByNumber($store->getId(), $orderNumber);
+        $allocation = $this->flowAllocation->getByNumber($store->getId(), $orderNumber);
+        $this->webhookEvent->processOrderPlacedPayloadData([
+            'allocation' => $allocation,
+            'order' => $order
+        ], false);
+    }
+
+    /**
+     * Allows logger to be overridden
+     * @param Logger $logger
+     */
+    public function setLogger(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+}

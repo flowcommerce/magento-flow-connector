200671babe6ba3654c3b569c3639ce280f2effc3
flowio-david <david@flow.io>
2020-12-07 11:40:50 -0500

    loadByIdWithoutStore

--- Model/WebhookEvent.php
+++ Model/WebhookEvent.php
@@ -2066,7 +2066,7 @@ class WebhookEvent extends AbstractModel implements WebhookEventInterface, Ident
         if (array_key_exists('attributes', $receivedOrder)) {
             if (array_key_exists(self::QUOTE_ID, $receivedOrder['attributes'])) {
                 $quoteId = $receivedOrder['attributes'][self::QUOTE_ID];
-                if ($userQuote = $this->quoteFactory->create()->load($quoteId)) {
+                if ($userQuote = $this->quoteFactory->create()->loadByIdWithoutStore($quoteId)) {
                     $userQuote->removeAllItems();
                 }
             }

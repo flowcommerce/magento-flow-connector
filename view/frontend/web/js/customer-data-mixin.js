aac51e5ced6f7cdec8dfcb27f84b5c746c985527
flowio-david <david@flow.io>
2020-12-08 12:13:04 -0500

    wip

--- view/frontend/web/js/customer-data-mixin.js
+++ view/frontend/web/js/customer-data-mixin.js
@@ -15,7 +15,7 @@ define([
             var config = {childList: true, subtree: true, characterData: true };
 
             var callback = function(mutationsList, observer) {
-                reloadFlowCart(observer, customerData);
+                reloadFlowCart(observer);
             };
 
             var observer = new MutationObserver(callback);
@@ -23,17 +23,13 @@ define([
             observer.observe(targetTotals, config);
         }
 
-        function reloadFlowCart(observer = null, customerData) {
+        function reloadFlowCart(observer = null) {
             if (observer) observer.disconnect();
             if (!flow.magento2.shouldLocalizeCart()) {
                 flow.magento2.showCart();
                 flow.magento2.showCartTotals();
                 return false;
             }
-
-            var sections = ['cart'];
-            customerData.invalidate(sections);
-            customerData.reload(sections, true);
 
             flow.magento2.hideCart();
             flow.magento2.hideCartTotals();

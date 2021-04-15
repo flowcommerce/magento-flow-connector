aca6e8d16c6f5fbf6b7caecc667e5847aac69e92
Rory Haddon <rory@flow.io>
2021-04-15 16:21:44 +0100

    [PET-2440] Fix organization race condition

--- view/frontend/web/js/flow-init.js
+++ view/frontend/web/js/flow-init.js
@@ -1,10 +1,10 @@
-define([], function () {
+define(['flow_organization_id'], function (flowOrganizationId) {
     window.flow = window.flow || {};
     window.flow.cmd = window.flow.cmd || function () {
         (window.flow.q = window.flow.q || []).push(arguments);
     };
 
-    window.flow.cmd('set', 'organization', window.flow_organization_id);
+    window.flow.cmd('set', 'organization', flowOrganizationId);
     window.flow.cmd('set', 'optinContainerSelector', '#flow-optin');
     window.flow.cmd('init');
 });

ea5eb2d0dc1f32902e1022dd023c99c8fc78bb63
flowio-david <david@flow.io>
2021-04-16 18:49:34 -0400

    wip

--- dev/travis/before_script.sh
+++ dev/travis/before_script.sh
@@ -4,7 +4,7 @@ set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 echo "==> Installing Magento 2 CE (Version $MAGENTO_VERSION) over composer create-project ..."
-composer self-update
+composer self-update --1
 cd $HOME
 composer create-project "magento/community-edition:$MAGENTO_VERSION" magento
 cd $HOME/magento
